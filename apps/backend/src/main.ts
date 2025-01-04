2124ea6a82aca1943e122c590f38537819f9c464
Madhuri Pokala <madhuri.pokala@experian.com>
2025-01-03 18:08:19 -0800

    updates cors policy

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,14 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS
   app.enableCors({
-    origin: (origin, callback) => {
-      // Allow requests from localhost or *.github.dev
-      if (!origin || /.*\.github\.dev$/.test(origin) || /^http:\/\/localhost(:\d+)?$/.test(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: '*',
     methods: 'GET,HEAD,POST,PUT,DELETE,PATCH',
     credentials: true,
   });
